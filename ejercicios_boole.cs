2e0dfdfb857bb644decddb7ba2dd54baccd57f20
Manuelaburiticagil <68049010+Manuelaburiticagil@users.noreply.github.com>
2020-07-23 16:13:17 -0500

    Add files via upload

---
+++ ejercicios_boole.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ejercicios_boole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese votos por partido a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese votos por partido b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese votos blancos");
+            int blancos = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese votos anulados");
+            int anulados = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el total de la población");
+            int totalPoblación = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el porcentaje de la población mayor de edad");
+            double porcentajeMayor = double.Parse(Console.ReadLine());
+
+            int votantes = a + b + blancos + anulados;
+            int abstención = (int)(((porcentajeMayor /100) * totalPoblación)-votantes);
+
+            bool A = anulados < 0.3 * (a + b);
+            bool B = (a + b) > blancos;
+            bool C = abstención < votantes;
+
+            if ((A || B) && C)
+            {
+                Console.WriteLine("las votaciones fueron exitosas");
+                if (a > b) Console.WriteLine("el partido ganador es a");
+                else Console.WriteLine("el partido ganador es b");
+            }
+            else Console.WriteLine(" las elecciones deben ser realizadas nuevamente");
+
+        }
+    }
+}
---
+++ switch_case.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su salario mensual");
+            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese su tipo de contrato: dependiente o independiente");
+            string tipodecontrato = Console.ReadLine();
+            Console.WriteLine("ingrese su nivel de riesgo");
+            double riesgo = double.Parse(Console.ReadLine());
+
+            double smmlv = 877803;
+            double baseCotización = 0.4 * salario;
+            if (baseCotización < smmlv) baseCotización = smmlv;
+            //switch para el tipo de contrato 
+            switch (tipodecontrato)
+            {
+                case "dependiente":
+                    double pensión = baseCotización * 0.04;
+                    double eps = baseCotización * 0.04;
+                    double salariomensual = (salario - pensión - eps);
+                    double salarioanual = (salariomensual * 12) + salario;
+                    Console.WriteLine("su salario mensual es:" + salariomensual + "y su salario anual es:" + salarioanual);
+                    break;
+
+                case "independiente":
+                    Console.WriteLine("Cuál es su nivel de riesgo");
+                    double ipensión = baseCotización * 0.16;
+                    double ieps =baseCotización * 0.125;
+
+                    //switch tipo de riesgo
+
+                    switch (riesgo)
+                    {
+                        case 1:
+                            double arl = baseCotización * 0.00522;
+                            break;
+
+                        case 2:
+                            double arl2 = baseCotización * 0.01044;
+                            break;
+
+                        case 3:
+                            double arl3 = baseCotización * 0.02436;
+                            break;
+
+                        case 4:
+                            double arl4 = baseCotización * 0.0435;
+                            break;
+
+                        case 5:
+                            double arl5 = baseCotización * 0.0696;
+                            break;
+
+
+
+                    }
+
+                    double salariomensuali = (salario - ipensión - ieps - riesgo);
+                    double salarioanuali = salariomensuali * 12;
+                    Console.WriteLine("su salario mensual es:" + salariomensuali + "y su salario anual es:" + salarioanuali);
+
+                    break;
+
+            }
+        }
+    }
+}
